86e64b7acaf8c14b545349c7d7a9ed443512f767
DUDU <DUDU@DUDU>
2024-07-15 18:12:46 +0530

    circleprogram2 created

---
+++ src/test/java/program/circleprog2.java
@@ -0,0 +1,19 @@
+package program;
+
+import java.util.Scanner;
+
+public class circleprog2 {
+
+	public static void main(String[] args) {
+		
+		final double pie=3.14;
+		System.out.println("Enter the radios");
+		Scanner sc = new Scanner(System.in);
+		 int d=sc.nextInt();
+		 double c=(pie*d*d);
+		 System.out.println(c);
+		 
+
+	}
+}
+
